ba79875060b9dd44dedbaee351fc9dcc86f8c33e
dmehdi544 <43506536+dmehdi544@users.noreply.github.com>
2018-09-23 03:52:50 +0330

    Update Mybot.php

--- Mybot.php
+++ Mybot.php
@@ -1,6 +1,33 @@
-include("Telegram.php");
-date_default_timezone_set("asia/tehran");
-// Set the bot 623068274:AAFF2uMxEigZlw2lKaHAYmS17L0_zQBQ0zg
-$bot_id = "623068274:AAFF2uMxEigZlw2lKaHAYmS17L0_zQBQ0zg";
-// Instances the class
-$telegram = new Telegram($bot_id);
+    include("Telegram.php");
+
+    date_default_timezone_set("asia/tehran");
+
+    // Set the bot TOKEN
+
+    $bot_id = "YOUR-TOKEN";
+
+    // Instances the class
+
+    $telegram = new Telegram($bot_id);
+
+    $text = $telegram->Text();
+
+    $username = $telegram->Username();
+
+    $name = $telegram->FirstName();
+
+    $family = $telegram->LastName();
+
+    $message_id = $telegram->MessageID();
+
+    $user_id = $telegram->UserID();
+
+    $chat_id = $telegram->ChatID();
+
+    if(!is_null($text) && !is_null($chat_id)){
+
+    $content = array('chat_id' => $chat_id, 'text' => $user_id);
+
+    $telegram->sendMessage($content); 
+
+    }
