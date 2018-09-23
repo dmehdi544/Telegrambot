8e5476cb89a61b23d2793e0a71d5864a184e3260
dmehdi544 <43506536+dmehdi544@users.noreply.github.com>
2018-09-23 04:05:10 +0330

    Update Mybot.php

--- Mybot.php
+++ Mybot.php
@@ -2,9 +2,9 @@
 
     date_default_timezone_set("asia/tehran");
 
-    // Set the bot TOKEN
+    // Set the bot 623068274:AAFF2uMxEigZlw2lKaHAYmS17L0_zQBQ0zg
 
-    $bot_id = "YOUR-TOKEN";
+    $bot_id = "623068274:AAFF2uMxEigZlw2lKaHAYmS17L0_zQBQ0zg";
 
     // Instances the class
 
